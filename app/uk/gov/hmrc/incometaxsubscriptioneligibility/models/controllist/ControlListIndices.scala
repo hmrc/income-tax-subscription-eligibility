f2f115c821b134e43868533da2aa56f4273d73bc
r-melvin <40235254+r-melvin@users.noreply.github.com>
2019-07-17 16:12:08 +0100

    SAR-4163: Added Control List parameters

---
+++ app/uk/gov/hmrc/incometaxsubscriptioneligibility/models/controllist/ControlListIndices.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxsubscriptioneligibility.models.controllist
+
+
+object ControlListIndices {
+  val NON_RESIDENT_COMPANY_LANDLORD = 0
+  val STUDENT_LOANS = 1
+  val MARRIAGE_ALLOWANCE = 2
+  val EMPLOYMENT_INCOME = 3
+  val COMPLIANCE = 4
+  val EXISTING_IDMS_DEBT = 5
+  val BUDGET_PAYMENT_PLAN = 6
+  val DECEASED = 7
+  val TIME_TO_PAY = 8
+  val SELF_SERVE_TIME_TO_PAY = 9
+  val NO_REPAYMENT = 10
+  val OUTSTANDING_RETURNS = 11
+  val RLS_CASE = 12
+  val ENFORCEMENT = 13
+  val COLLECTION_PRIORITY = 14
+  val FAILURE_TO_NOTIFY = 15
+  val BANKRUPT_INSOLVENT = 16
+  val BANKRUPT_VOLUNTARY_ARRANGEMENT = 17
+  val MULTIPLE_SELF_EMPLOYED = 18
+  val NON_RESIDENTS = 19
+  val MINISTERS_OF_RELIGION = 20
+  val LLOYDS_UNDERWRITER = 21
+  val CLASS_TWO_NATIONAL_INSURANCE_CONTRIBUTIONS_VOLUNTARY = 22
+  val BANKS_AND_BUILDING_SOCIETY_INTEREST_FOREIGN = 23
+  val DIVIDENDS_FOREIGN = 24
+  val PENSION_CONTRIBUTIONS = 25
+  val PENSION_INCOME = 26
+  val CONSTRUCTION_INDUSTRY_SCHEME_DEDUCTIONS = 27
+  val SA101_ADDITIONAL_INFORMATION = 28
+  val AVERAGING_ADJUSTMENT = 29
+  val CAPITAL_GAINS_TAX = 30
+  val FOREIGN_INCOME = 31
+  val TRUST_INCOME = 32
+  val PARTNERSHIP_INCOME = 33
+  val HIGH_INCOME_CHILD_BENEFIT = 34
+  val BLIND_PERSONS_ALLOWANCE = 35
+  val FOSTER_CARERS = 36
+  val MARRIED_COUPLES_ALLOWANCE = 37
+  val CAPACITOR = 38
+  val DISGUISED_RENUMERATION_INVOLVEMENT = 39
+}
