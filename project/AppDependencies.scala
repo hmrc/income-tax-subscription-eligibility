e8b5e42bf097bf2b37938a552324bc1ed22e673c
Alex Rimmer <31283208+AlexRimmerHMRC@users.noreply.github.com>
2023-12-18 10:06:22 +0000

    ITSASU-2781 - Update to play 3.0 and update relevant dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,28 +2,19 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.13.0"
-  private val domainVersion = "8.1.0-play-28"
-  private val scalaTestVersion = "3.2.14"
-  private val playCurrent = "2.8.17"
+  private val bootstrapVersion = "8.2.0"
+  private val domainVersion = "9.0.0"
 
-  val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.4",
-    "uk.gov.hmrc" %% "domain" % domainVersion
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc" %% "domain-play-30" % domainVersion
   )
 
-  val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % "test, it",
-    "com.typesafe.play" %% "play-test" % playCurrent % "test",
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % "test",
     "org.scalamock" %% "scalamock" % "5.2.0" % "test",
-    "org.scalatest" %% "scalatest" % scalaTestVersion % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
-    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % "test, it", // NB Added because scalatest requires it, but does not declare a dependency
-    "org.mockito" % "mockito-core" % "4.8.0" % "test, it",
-    "org.scalatestplus" %% "mockito-3-12" % "3.2.10.0" % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.32.0" % "it"
-
   )
+
+  val itTest: Seq[ModuleID] = Seq()
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,15 +3,15 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefact
 
 resolvers += "HMRC open artefacts Maven2 Repository" at "https://open.artefacts.tax.service.gov.uk/maven2"
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.9.0")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.2.0")
-
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.18")
-
 resolvers += Resolver.url("scoverage-bintray", url("https://dl.bintray.com/sksamuel/sbt-plugins/"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.3")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.17.0")
 
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.4.0")
+
+addSbtPlugin("org.playframework" % "sbt-plugin" % "3.0.0")
+
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.9")
+
+addSbtPlugin("org.scalastyle" % "scalastyle-sbt-plugin" % "1.0.0" exclude("org.scala-lang.modules", "scala-xml_2.12"))
 
